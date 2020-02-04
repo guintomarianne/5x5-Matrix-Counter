5ed88702c34f4ba3601df791cdc0f26c95e238ef
guintomarianne <guintomarianne@gmail.com>
2020-02-04 21:56:35 +0800

    First Commit

---
+++ 5x5-Matrix-Counter/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.Label23 = New System.Windows.Forms.Label()
+        Me.Label24 = New System.Windows.Forms.Label()
+        Me.Label25 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label1.Location = New System.Drawing.Point(44, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 40)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label2.Location = New System.Drawing.Point(124, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 40)
+        Me.Label2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label3.Location = New System.Drawing.Point(204, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 40)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label4.Location = New System.Drawing.Point(283, 42)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 40)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label5.Location = New System.Drawing.Point(361, 42)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 40)
+        Me.Label5.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label6.Location = New System.Drawing.Point(361, 122)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 40)
+        Me.Label6.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label7.Location = New System.Drawing.Point(283, 122)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 40)
+        Me.Label7.TabIndex = 8
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label8.Location = New System.Drawing.Point(204, 122)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 40)
+        Me.Label8.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label9.Location = New System.Drawing.Point(124, 122)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(40, 40)
+        Me.Label9.TabIndex = 6
+        '
+        'Label10
+        '
+        Me.Label10.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label10.Location = New System.Drawing.Point(44, 122)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(40, 40)
+        Me.Label10.TabIndex = 5
+        '
+        'Label11
+        '
+        Me.Label11.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label11.Location = New System.Drawing.Point(361, 354)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(40, 40)
+        Me.Label11.TabIndex = 14
+        '
+        'Label12
+        '
+        Me.Label12.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label12.Location = New System.Drawing.Point(283, 354)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(40, 40)
+        Me.Label12.TabIndex = 13
+        '
+        'Label13
+        '
+        Me.Label13.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label13.Location = New System.Drawing.Point(204, 354)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(40, 40)
+        Me.Label13.TabIndex = 12
+        '
+        'Label14
+        '
+        Me.Label14.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label14.Location = New System.Drawing.Point(124, 354)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(40, 40)
+        Me.Label14.TabIndex = 11
+        '
+        'Label15
+        '
+        Me.Label15.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label15.Location = New System.Drawing.Point(44, 354)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(40, 40)
+        Me.Label15.TabIndex = 10
+        '
+        'Label16
+        '
+        Me.Label16.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label16.Location = New System.Drawing.Point(361, 281)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(40, 40)
+        Me.Label16.TabIndex = 24
+        '
+        'Label17
+        '
+        Me.Label17.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label17.Location = New System.Drawing.Point(283, 281)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(40, 40)
+        Me.Label17.TabIndex = 23
+        '
+        'Label18
+        '
+        Me.Label18.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label18.Location = New System.Drawing.Point(204, 281)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(40, 40)
+        Me.Label18.TabIndex = 22
+        '
+        'Label19
+        '
+        Me.Label19.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label19.Location = New System.Drawing.Point(124, 281)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(40, 40)
+        Me.Label19.TabIndex = 21
+        '
+        'Label20
+        '
+        Me.Label20.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label20.Location = New System.Drawing.Point(44, 281)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(40, 40)
+        Me.Label20.TabIndex = 20
+        '
+        'Label21
+        '
+        Me.Label21.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label21.Location = New System.Drawing.Point(361, 201)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(40, 40)
+        Me.Label21.TabIndex = 19
+        '
+        'Label22
+        '
+        Me.Label22.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label22.Location = New System.Drawing.Point(283, 201)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(40, 40)
+        Me.Label22.TabIndex = 18
+        '
+        'Label23
+        '
+        Me.Label23.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label23.Location = New System.Drawing.Point(204, 201)
+        Me.Label23.Name = "Label23"
+        Me.Label23.Size = New System.Drawing.Size(40, 40)
+        Me.Label23.TabIndex = 17
+        '
+        'Label24
+        '
+        Me.Label24.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label24.Location = New System.Drawing.Point(124, 201)
+        Me.Label24.Name = "Label24"
+        Me.Label24.Size = New System.Drawing.Size(40, 40)
+        Me.Label24.TabIndex = 16
+        '
+        'Label25
+        '
+        Me.Label25.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Label25.Location = New System.Drawing.Point(44, 201)
+        Me.Label25.Name = "Label25"
+        Me.Label25.Size = New System.Drawing.Size(40, 40)
+        Me.Label25.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(447, 441)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label23)
+        Me.Controls.Add(Me.Label24)
+        Me.Controls.Add(Me.Label25)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "5x5 Matrix Counter"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label25 As Label
+End Class
