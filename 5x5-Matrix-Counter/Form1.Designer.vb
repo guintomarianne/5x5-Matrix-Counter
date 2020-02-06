e581d6379f6bb7d20eebc1304aefb6f613849631
guintomarianne <guintomarianne@gmail.com>
2020-02-06 20:10:25 +0800

    Second Commit - Counting

--- 5x5-Matrix-Counter/Form1.Designer.vb
+++ 5x5-Matrix-Counter/Form1.Designer.vb
@@ -22,11 +22,15 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
@@ -47,6 +51,7 @@ Partial Class Form1
         Me.Label23 = New System.Windows.Forms.Label()
         Me.Label24 = New System.Windows.Forms.Label()
         Me.Label25 = New System.Windows.Forms.Label()
+        Me.TimerLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -89,21 +94,49 @@ Partial Class Form1
         Me.Label5.Size = New System.Drawing.Size(40, 40)
         Me.Label5.TabIndex = 4
         '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SeaGreen
+        Me.Button1.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Button1.Location = New System.Drawing.Point(477, 85)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 77)
+        Me.Button1.TabIndex = 26
+        Me.Button1.Text = "START"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Brown
+        Me.Button2.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Button2.Location = New System.Drawing.Point(477, 244)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 77)
+        Me.Button2.TabIndex = 27
+        Me.Button2.Text = "STOP"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
         'Label6
         '
         Me.Label6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label6.Location = New System.Drawing.Point(361, 122)
+        Me.Label6.Location = New System.Drawing.Point(44, 121)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(40, 40)
-        Me.Label6.TabIndex = 9
+        Me.Label6.TabIndex = 28
         '
         'Label7
         '
         Me.Label7.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label7.Location = New System.Drawing.Point(283, 122)
+        Me.Label7.Location = New System.Drawing.Point(124, 122)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(40, 40)
-        Me.Label7.TabIndex = 8
+        Me.Label7.TabIndex = 29
         '
         'Label8
         '
@@ -111,79 +144,79 @@ Partial Class Form1
         Me.Label8.Location = New System.Drawing.Point(204, 122)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(40, 40)
-        Me.Label8.TabIndex = 7
+        Me.Label8.TabIndex = 30
         '
         'Label9
         '
         Me.Label9.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label9.Location = New System.Drawing.Point(124, 122)
+        Me.Label9.Location = New System.Drawing.Point(283, 122)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(40, 40)
-        Me.Label9.TabIndex = 6
+        Me.Label9.TabIndex = 31
         '
         'Label10
         '
         Me.Label10.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label10.Location = New System.Drawing.Point(44, 122)
+        Me.Label10.Location = New System.Drawing.Point(361, 122)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(40, 40)
-        Me.Label10.TabIndex = 5
+        Me.Label10.TabIndex = 32
         '
         'Label11
         '
         Me.Label11.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label11.Location = New System.Drawing.Point(361, 354)
+        Me.Label11.Location = New System.Drawing.Point(44, 197)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(40, 40)
-        Me.Label11.TabIndex = 14
+        Me.Label11.TabIndex = 33
         '
         'Label12
         '
         Me.Label12.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label12.Location = New System.Drawing.Point(283, 354)
+        Me.Label12.Location = New System.Drawing.Point(124, 197)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(40, 40)
-        Me.Label12.TabIndex = 13
+        Me.Label12.TabIndex = 34
         '
         'Label13
         '
         Me.Label13.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label13.Location = New System.Drawing.Point(204, 354)
+        Me.Label13.Location = New System.Drawing.Point(204, 197)
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(40, 40)
-        Me.Label13.TabIndex = 12
+        Me.Label13.TabIndex = 35
         '
         'Label14
         '
         Me.Label14.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label14.Location = New System.Drawing.Point(124, 354)
+        Me.Label14.Location = New System.Drawing.Point(283, 197)
         Me.Label14.Name = "Label14"
         Me.Label14.Size = New System.Drawing.Size(40, 40)
-        Me.Label14.TabIndex = 11
+        Me.Label14.TabIndex = 36
         '
         'Label15
         '
         Me.Label15.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label15.Location = New System.Drawing.Point(44, 354)
+        Me.Label15.Location = New System.Drawing.Point(361, 197)
         Me.Label15.Name = "Label15"
         Me.Label15.Size = New System.Drawing.Size(40, 40)
-        Me.Label15.TabIndex = 10
+        Me.Label15.TabIndex = 37
         '
         'Label16
         '
         Me.Label16.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label16.Location = New System.Drawing.Point(361, 281)
+        Me.Label16.Location = New System.Drawing.Point(44, 280)
         Me.Label16.Name = "Label16"
         Me.Label16.Size = New System.Drawing.Size(40, 40)
-        Me.Label16.TabIndex = 24
+        Me.Label16.TabIndex = 38
         '
         'Label17
         '
         Me.Label17.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label17.Location = New System.Drawing.Point(283, 281)
+        Me.Label17.Location = New System.Drawing.Point(124, 281)
         Me.Label17.Name = "Label17"
         Me.Label17.Size = New System.Drawing.Size(40, 40)
-        Me.Label17.TabIndex = 23
+        Me.Label17.TabIndex = 39
         '
         'Label18
         '
@@ -191,89 +224,102 @@ Partial Class Form1
         Me.Label18.Location = New System.Drawing.Point(204, 281)
         Me.Label18.Name = "Label18"
         Me.Label18.Size = New System.Drawing.Size(40, 40)
-        Me.Label18.TabIndex = 22
+        Me.Label18.TabIndex = 40
         '
         'Label19
         '
         Me.Label19.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label19.Location = New System.Drawing.Point(124, 281)
+        Me.Label19.Location = New System.Drawing.Point(283, 281)
         Me.Label19.Name = "Label19"
         Me.Label19.Size = New System.Drawing.Size(40, 40)
-        Me.Label19.TabIndex = 21
+        Me.Label19.TabIndex = 41
         '
         'Label20
         '
         Me.Label20.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label20.Location = New System.Drawing.Point(44, 281)
+        Me.Label20.Location = New System.Drawing.Point(361, 281)
         Me.Label20.Name = "Label20"
         Me.Label20.Size = New System.Drawing.Size(40, 40)
-        Me.Label20.TabIndex = 20
+        Me.Label20.TabIndex = 42
         '
         'Label21
         '
         Me.Label21.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label21.Location = New System.Drawing.Point(361, 201)
+        Me.Label21.Location = New System.Drawing.Point(44, 355)
         Me.Label21.Name = "Label21"
         Me.Label21.Size = New System.Drawing.Size(40, 40)
-        Me.Label21.TabIndex = 19
+        Me.Label21.TabIndex = 43
         '
         'Label22
         '
         Me.Label22.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label22.Location = New System.Drawing.Point(283, 201)
+        Me.Label22.Location = New System.Drawing.Point(124, 355)
         Me.Label22.Name = "Label22"
         Me.Label22.Size = New System.Drawing.Size(40, 40)
-        Me.Label22.TabIndex = 18
+        Me.Label22.TabIndex = 44
         '
         'Label23
         '
         Me.Label23.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label23.Location = New System.Drawing.Point(204, 201)
+        Me.Label23.Location = New System.Drawing.Point(204, 355)
         Me.Label23.Name = "Label23"
         Me.Label23.Size = New System.Drawing.Size(40, 40)
-        Me.Label23.TabIndex = 17
+        Me.Label23.TabIndex = 45
         '
         'Label24
         '
         Me.Label24.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label24.Location = New System.Drawing.Point(124, 201)
+        Me.Label24.Location = New System.Drawing.Point(283, 355)
         Me.Label24.Name = "Label24"
         Me.Label24.Size = New System.Drawing.Size(40, 40)
-        Me.Label24.TabIndex = 16
+        Me.Label24.TabIndex = 46
         '
         'Label25
         '
         Me.Label25.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
-        Me.Label25.Location = New System.Drawing.Point(44, 201)
+        Me.Label25.Location = New System.Drawing.Point(361, 355)
         Me.Label25.Name = "Label25"
         Me.Label25.Size = New System.Drawing.Size(40, 40)
-        Me.Label25.TabIndex = 15
+        Me.Label25.TabIndex = 47
+        '
+        'TimerLabel
+        '
+        Me.TimerLabel.AutoSize = True
+        Me.TimerLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TimerLabel.Location = New System.Drawing.Point(511, 411)
+        Me.TimerLabel.Name = "TimerLabel"
+        Me.TimerLabel.Size = New System.Drawing.Size(3, 2)
+        Me.TimerLabel.TabIndex = 48
+        Me.TimerLabel.Text = "0"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(447, 441)
-        Me.Controls.Add(Me.Label16)
-        Me.Controls.Add(Me.Label17)
-        Me.Controls.Add(Me.Label18)
-        Me.Controls.Add(Me.Label19)
-        Me.Controls.Add(Me.Label20)
-        Me.Controls.Add(Me.Label21)
-        Me.Controls.Add(Me.Label22)
-        Me.Controls.Add(Me.Label23)
-        Me.Controls.Add(Me.Label24)
+        Me.ClientSize = New System.Drawing.Size(665, 441)
+        Me.Controls.Add(Me.TimerLabel)
         Me.Controls.Add(Me.Label25)
-        Me.Controls.Add(Me.Label11)
-        Me.Controls.Add(Me.Label12)
-        Me.Controls.Add(Me.Label13)
-        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label24)
+        Me.Controls.Add(Me.Label23)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
         Me.Controls.Add(Me.Label15)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -282,6 +328,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "5x5 Matrix Counter"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -290,6 +337,9 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
@@ -310,4 +360,5 @@ Partial Class Form1
     Friend WithEvents Label23 As Label
     Friend WithEvents Label24 As Label
     Friend WithEvents Label25 As Label
+    Friend WithEvents TimerLabel As Label
 End Class
--- 5x5-Matrix-Counter/Form1.vb
+++ 5x5-Matrix-Counter/Form1.vb
@@ -1,3 +1,300 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TimerLabel.Text += 1
+
+        If TimerLabel.Text = 1 Then
+            Label1.BackColor = Color.LightBlue
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.LightBlue
+            Label5.BackColor = Color.LightBlue
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.OrangeRed
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.LightBlue
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.LightBlue
+            Label15.BackColor = Color.LightBlue
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.OrangeRed
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.LightBlue
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.LightBlue
+
+        ElseIf TimerLabel.Text = 2 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.OrangeRed
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.OrangeRed
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.OrangeRed
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.LightBlue
+            Label21.BackColor = Color.OrangeRed
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.OrangeRed
+
+        ElseIf TimerLabel.Text = 3 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.OrangeRed
+            Label11.BackColor = Color.LightBlue
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.OrangeRed
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.OrangeRed
+
+        ElseIf TimerLabel.Text = 4 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.LightBlue
+            Label3.BackColor = Color.LightBlue
+            Label4.BackColor = Color.LightBlue
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.OrangeRed
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.OrangeRed
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.OrangeRed
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.LightBlue
+            Label23.BackColor = Color.LightBlue
+            Label24.BackColor = Color.LightBlue
+            Label25.BackColor = Color.OrangeRed
+
+        ElseIf TimerLabel.Text = 5 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.OrangeRed
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.OrangeRed
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.OrangeRed
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.OrangeRed
+
+        ElseIf TimerLabel.Text = 6 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.OrangeRed
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.OrangeRed
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.OrangeRed
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.OrangeRed
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.OrangeRed
+
+        ElseIf TimerLabel.Text = 7 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.OrangeRed
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.LightBlue
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.LightBlue
+            Label15.BackColor = Color.LightBlue
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.OrangeRed
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.LightBlue
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.LightBlue
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.LightBlue
+            Label25.BackColor = Color.LightBlue
+
+        ElseIf TimerLabel.Text = 8 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.OrangeRed
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.OrangeRed
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.OrangeRed
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.OrangeRed
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.OrangeRed
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.OrangeRed
+
+        ElseIf TimerLabel.Text = 9 Then
+            Label1.BackColor = Color.OrangeRed
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.OrangeRed
+            Label6.BackColor = Color.OrangeRed
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.OrangeRed
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.OrangeRed
+            Label13.BackColor = Color.OrangeRed
+            Label14.BackColor = Color.OrangeRed
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.OrangeRed
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.OrangeRed
+
+        Else
+            TimerLabel.Text = 0
+
+            Label1.BackColor = Color.LightBlue
+            Label2.BackColor = Color.OrangeRed
+            Label3.BackColor = Color.OrangeRed
+            Label4.BackColor = Color.OrangeRed
+            Label5.BackColor = Color.LightBlue
+            Label6.BackColor = Color.OrangeRed
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.OrangeRed
+            Label11.BackColor = Color.OrangeRed
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.LightBlue
+            Label14.BackColor = Color.LightBlue
+            Label15.BackColor = Color.OrangeRed
+            Label16.BackColor = Color.OrangeRed
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.OrangeRed
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.OrangeRed
+            Label23.BackColor = Color.OrangeRed
+            Label24.BackColor = Color.OrangeRed
+            Label25.BackColor = Color.LightBlue
+
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Timer1.Stop()
+
+    End Sub
+
 
 End Class
